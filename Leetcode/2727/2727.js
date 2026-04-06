126a411bccf60e2e9067a9c068a2b70d5c526aad
Leanderson <leandersonsouzaultra@gmail.com>
2026-04-06 12:42:19 -0300

    adding some exercises from leetcode

---
+++ Leetcode/2727/2727.js
@@ -0,0 +1,13 @@
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    let k = obj;
+    if (k==null)
+        return false;
+    else
+        return true;
+
+    
+};
